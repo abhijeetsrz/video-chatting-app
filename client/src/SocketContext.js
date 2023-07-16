bcc5ba364f970cb3d53b92079a9b1cfc8df70478
Abhijeet <abhi200122002@gmail.com>
2023-07-16 23:59:19 +0530

    adding server to client

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('video-chatting-app-seven.vercel.app');
 
 
 const ContextProvider = ({ children }) => {
